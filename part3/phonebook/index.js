9d4839557f09e96677e530e4249082f2923276d2
megaeonise <54655406+megaeonise@users.noreply.github.com>
2024-12-31 19:58:10 +0600

    up to 3.6 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,74 +3,78 @@ const app = express()
 
 app.use(express.json())
 
-let notes = [
-  {
-    id: '1',
-    content: "HTML is easy",
-    important: true
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
   },
-  {
-    id: '2',
-    content: "Browser can execute only JavaScript",
-    important: false
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
   },
-  {
-    id: '3',
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
   }
 ]
-app.get('/api/notes/:id', (req, res) => {
-    const id = req.params.id
-    console.log(id)
-    const note = notes.find(note => note.id === id)
-    if(note){
-        res.json(note)
-    }
-    else{
-        res.status(404).end()
-    }
-    
-})
 
   
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
+app.post('/api/persons', (req, res) => {
+  const id = Math.floor(Math.random() * 10000)
+  console.log(id)
+  const {name, number} = req.body
+  const error = persons.find(person => person.name === name)
+  if(persons.find(person => person.name === name)){
+    console.log('?')
+    res.send('name must be unique')
+  }
+  else if(name && number){
+    const person = {id, name, number}
+    console.log(person)
+    res.json(person)
+  }
+  else if(!name && !number){
+    res.send('name and number is missing')
+  }
+  else if(!name){
+    res.send('name is missing')
+  }
+  else{
+    res.send('number is missing')
   }
   
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
-  })
+})
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = req.params.id
-    console.log(id)
-    notes = notes.filter(note => note.id !== id)
-    res.status(204).end()
-    
+
+
+app.get('/info', (req, res) => {
+  const today = new Date()
+  const entry_num = persons.length
+  res.send(`Phonebook has info for ${entry_num} people <br> ${today}`)
+}) 
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person =  persons.find(person => person.id === id)
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
 })
 
 const PORT = 3001
