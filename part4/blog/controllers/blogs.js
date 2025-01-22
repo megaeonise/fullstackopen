b5692114437445d2367e4c987740adf01633daf2
megaeonise <54655406+megaeonise@users.noreply.github.com>
2025-01-23 04:26:26 +0600

    4.19 done

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,7 +2,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const logger = require('../utils/logger')
+const jwt = require('jsonwebtoken')
 
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/blogs', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
@@ -10,7 +19,12 @@ blogsRouter.get('/blogs', async (request, response) => {
 })
 
 blogsRouter.post('/blogs', async (request, response) => {
-  const user = await User.findById(request.body.user)
+  
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
