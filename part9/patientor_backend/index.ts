bbdea0a6af8fa1b6cc613c30509b185602f78669
megaeonise <54655406+megaeonise@users.noreply.github.com>
2025-05-08 22:55:07 +0600

    9.10 and 9.11 done

--- part9/patientor_backend/index.ts
+++ part9/patientor_backend/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import diagnosisRouter from './routes/diagnosis'
+import patientRouter from './routes/patient'
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const PORT = 3001;
 
@@ -8,6 +12,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
